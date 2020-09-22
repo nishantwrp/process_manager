a61d65073c4dedface7b0e4c97ac8d0acde8b29f
Nishant Mittal <mittalnishant14@outlook.com>
2020-09-23 03:41:39 +0530

    remove heroku

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py migrate
-web: gunicorn process_manager.wsgi
--- process_manager/settings.py
+++ process_manager/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -146,6 +145,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
